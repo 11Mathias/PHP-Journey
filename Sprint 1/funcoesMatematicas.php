4092b1f5b29bfa01eebcbd9d8c47f1931f6b5f47
11Mathias <1prodkan@gmail.com>
2025-01-15 17:52:25 -0300

    First commit

---
+++ Sprint 1/funcoesMatematicas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <?php
+        //funções matematicas 
+        $A = $_GET["a"];
+        $B = $_GET["b"];
+        echo "Valores recebidos, $A e $B";
+        echo "<br/>O valor absoluto de $A é " . abs($A);
+        echo "<br/>A potencia de $A<sup>$A</sup> é " . pow($A, $A);
+        echo "<br/>A raiz de $A é " . sqrt($A);
+        echo "<br/>Arredondando $B fica " . round($B);
+
+        ?>
+    </div>
+</body>
+
+</html>
